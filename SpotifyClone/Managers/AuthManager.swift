844132d4b25105efdbc408c8f2e8737bfe915564
Kodai <system@Kodai-HayashinoMacBook.local>
2021-10-27 19:12:53 +0900

    loginのためのauthの設定を行いました

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientID = "9a2ce23be2f94e11ba64800b0b857f70"
+        static let clientSecret = "52e16da4a58e4cea81c11bf4027da65b"
+    }
+    
+    private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURL = "https://note.com/erizabesu_japan/n/n8821ebab9925"
+        let base = "https://accounts.spotify.com/authorize"
+        let string =
+            "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURL)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+    
+    var isSignedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String? {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool? {
+        return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        // Get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
+    }
+}
